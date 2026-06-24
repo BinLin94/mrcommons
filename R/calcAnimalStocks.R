5c206290b2572dd85b349cf0ff38076eb6e2323f
Bin Lin <aviva_lin@126.com>
2026-06-24 14:45:18 +0200

    update calcAnimalStocks for new FAO 2024 data format

--- R/calcAnimalStocks.R
+++ R/calcAnimalStocks.R
@@ -4,7 +4,7 @@
 #' @param grouping IPCC: Animal grouping of IPCC Guidelines
 #'
 #' @return List of magpie objects with results on country level, weight on country level, unit and description.
-#' @author Benjamin Leon Bodirsky
+#' @author Benjamin Leon Bodirsky, Bin Lin
 #' @seealso
 #' [calcExcretionIPCC()],
 #' [readIPCC()]
@@ -20,107 +20,111 @@ calcAnimalStocks <- function(grouping = "IPCC") {
 
   marketSwineShare <- 0.9 # table 10.19
 
-  liveHead <- dimSums(readSource("FAO_online", "LiveHead"), dim = "ElementShort")
-  livePrim <- readSource("FAO_online", "LivePrim")
-  years <- intersect(getYears(liveHead), getYears(livePrim))
-  liveHead <- liveHead[, years, ]
-  livePrim <- livePrim[, years, ]
+  # LiveHead and LivePrim were merged into Production_Crops_Livestock in 2024.
+  # Read once; liveHead aggregates over ElementShort, fao retains full structure
+  # for item-element combinations (format: "ItemCodeItem.ElementShort").
+  # Old approach (two separate reads, data up to 2019):
+  # liveHead <- dimSums(readSource("FAO_online", "LiveHead"), dim = "ElementShort")
+  # livePrim <- readSource("FAO_online", "LivePrim")
+  fao      <- readSource("FAO_online", "LiveHead")
+  liveHead <- dimSums(fao, dim = "ElementShort")
 
   # estimate numbers of animals for IPCC categories
   animals <- NULL
 
   # Dairy cows
-  animals <- mbind(animals, setNames(collapseNames(livePrim[, , c("Milk_Animals_(Head)")]
-  [, , c("882|Milk, whole fresh cow")]), "dairy cows"))
+  animals <- mbind(animals, setNames(
+    collapseNames(fao[, , "882|Raw milk of cattle.Milk_Animals_(An)"]), "dairy cows"))
   # Other cattle
   animals <- mbind(animals, setNames(
-    collapseNames(liveHead[, , c("866|Cattle")])
+    collapseNames(liveHead[, , "866|Cattle"])
     - setNames(animals[, , "dairy cows"], NULL),
     "other cattle"
   ))
 
   # Dairy Buffalo
-  animals <- mbind(animals, setNames(collapseNames(livePrim[, , c("Milk_Animals_(Head)")]
-                                                   [, , c("951|Milk, whole fresh buffalo")]), "dairy buffalo"))
+  animals <- mbind(animals, setNames(
+    collapseNames(fao[, , "951|Raw milk of buffalo.Milk_Animals_(An)"]), "dairy buffalo"))
   # Other buffalo
-  animals <- mbind(animals, setNames(collapseNames(liveHead[, , c("946|Buffaloes")]) -
-                                       setNames(animals[, , "dairy buffalo"], NULL), "other buffalo"))
+  animals <- mbind(animals, setNames(
+    collapseNames(liveHead[, , "946|Buffalo"])
+    - setNames(animals[, , "dairy buffalo"], NULL), "other buffalo"))
 
   # Market Swine
   animals <- mbind(animals, setNames(
-    collapseNames(liveHead[, , c("1034|Pigs")])
-    * marketSwineShare,
+    collapseNames(liveHead[, , "1034|Swine / pigs"]) * marketSwineShare,
     "market swine"
   ))
 
   # Breeding Swine
   animals <- mbind(animals, setNames(
-    collapseNames(liveHead[, , c("1034|Pigs")])
-    * (1 - marketSwineShare),
+    collapseNames(liveHead[, , "1034|Swine / pigs"]) * (1 - marketSwineShare),
     "breeding swine"
   ))
 
   # Dairy Sheep
-  animals <- mbind(animals, setNames(collapseNames(livePrim[, , c("Milk_Animals_(Head)")]
-  [, , c("982|Milk, whole fresh sheep")]), "dairy sheep"))
+  animals <- mbind(animals, setNames(
+    collapseNames(fao[, , "982|Raw milk of sheep.Milk_Animals_(An)"]), "dairy sheep"))
   # Other sheep
   animals <- mbind(animals, setNames(
-    collapseNames(liveHead[, , c("976|Sheep")])
+    collapseNames(liveHead[, , "976|Sheep"])
     - setNames(animals[, , "dairy sheep"], NULL),
     "other sheep"
   ))
 
   # Dairy Goats
-  animals <- mbind(animals, setNames(collapseNames(livePrim[, , c("Milk_Animals_(Head)")]
-  [, , c("1020|Milk, whole fresh goat")]), "dairy goats"))
+  animals <- mbind(animals, setNames(
+    collapseNames(fao[, , "1020|Raw milk of goats.Milk_Animals_(An)"]), "dairy goats"))
   # Other goats
   animals <- mbind(animals, setNames(
-    collapseNames(liveHead[, , c("1016|Goats")])
+    collapseNames(liveHead[, , "1016|Goats"])
     - setNames(animals[, , "dairy goats"], NULL),
     "other goats"
   ))
 
-  # Camels
-  animals <- mbind(animals, setNames(collapseNames(livePrim[, , c("Milk_Animals_(Head)")]
-  [, , c("1130|Milk, whole fresh camel")]), "dairy camels"))
+  # Dairy Camels
+  animals <- mbind(animals, setNames(
+    collapseNames(fao[, , "1130|Raw milk of camel.Milk_Animals_(An)"]), "dairy camels"))
   # Other Camelids
   animals <- mbind(animals, setNames(
-    dimSums(liveHead[, , c("1126|Camels", "1157|Camelids, other")], dim = 3.1)
+    dimSums(liveHead[, , c("1126|Camels", "1157|Other camelids")], dim = 3.1)
     - setNames(animals[, , "dairy camels"], NULL),
     "other camels"
   ))
 
   # Horses
   animals <- mbind(animals, setNames(
-    collapseNames(liveHead[, , c("1096|Horses")]),
+    collapseNames(liveHead[, , "1096|Horses"]),
     "horses"
   ))
 
   # Mules and Asses
   animals <- mbind(animals, setNames(
-    dimSums(liveHead[, , c("1107|Asses", "1110|Mules")], dim = 3.1),
+    dimSums(liveHead[, , c("1107|Asses", "1110|Mules and hinnies")], dim = 3.1),
     "mules and asses"
   ))
 
   # Poultry Layers
-  animals <- mbind(animals, setNames(collapseNames(dimSums(livePrim[, , c("Laying_(Head)")]
-  [, , c("1062|Eggs, hen, in shell", "1091|Eggs, other bird, in shell")], dim = 3.1)), "poultry layers"))
+  animals <- mbind(animals, setNames(
+    dimSums(fao[, , c("1062|Hen eggs in shell, fresh.Laying_(An)",
+                      "1091|Eggs from other birds in shell, fresh, nec.Laying_(An)")], dim = 3),
+    "poultry layers"))
   # Broilers
   animals <- mbind(animals, setNames(
-    dimSums(liveHead[, , c("1057|Chickens", "1083|Pigeons, other birds")], dim = 3.1)
+    dimSums(liveHead[, , c("1057|Chickens", "1083|Other birds")], dim = 3.1)
     - setNames(animals[, , "poultry layers"], NULL),
     "broilers"
   ))
 
   # Turkey
   animals <- mbind(animals, setNames(
-    dimSums(liveHead[, , c("1072|Geese and guinea fowls", "1079|Turkeys")], dim = 3.1),
+    dimSums(liveHead[, , c("1072|Geese", "1079|Turkeys")], dim = 3.1),
     "turkey"
   ))
 
   # Ducks
   animals <- mbind(animals, setNames(
-    collapseNames(liveHead[, , c("1068|Ducks")]),
+    collapseNames(liveHead[, , "1068|Ducks"]),
     "ducks"
   ))
 
